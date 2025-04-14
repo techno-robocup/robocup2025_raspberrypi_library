078a290d6b618a34b89b52bc64fa9659913aeca4
rotarymars <s.goto2050@gmail.com>
2025-04-14 17:18:58 +0900

    Removed variable initialization

--- camera.py
+++ camera.py
@@ -38,19 +38,3 @@ class Camera:
     def stop_cam(self):
         self.cam.stop()
 
-
-Rescue_Camera = Camera(
-    PORT=modules.settings.Rescue_Camera_PORT,
-    controls=modules.settings.Rescue_Camera_Controls,
-    size=modules.settings.Rescue_Camera_size,
-    formats=modules.settings.Rescue_Camera_formats,
-    lores_size=modules.settings.Rescue_Camera_lores_size,
-    pre_callback_func=modules.settings.Rescue_Camera_Pre_Callback_func)
-
-Linetrace_Camera = Camera(
-    PORT=modules.settings.Linetrace_Camera_PORT,
-    controls=modules.settings.Linetrace_Camera_Controls,
-    size=modules.settings.Linetrace_Camera_size,
-    formats=modules.settings.Linetrace_Camera_formats,
-    lores_size=modules.settings.Linetrace_Camera_lores_size,
-    pre_callback_func=modules.settings.Linetrace_Camera_Pre_Callback_func)
