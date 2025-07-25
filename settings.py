813fe3ad1276e4b5a834c8a5344875087bac285e
rotarymars <s.goto2050@gmail.com>
2025-07-25 16:00:26 +0900

    changed port of camera

--- settings.py
+++ settings.py
@@ -417,7 +417,7 @@ def Rescue_Camera_Pre_callback(request):
     print("Rescue_Camera_Pre_callback")
 
 
-Rescue_Camera_PORT = 1
+Rescue_Camera_PORT = 0
 Rescue_Camera_Controls = {
     "AfMode": controls.AfModeEnum.Continuous,
     "AfSpeed": controls.AfSpeedEnum.Fast,
@@ -434,7 +434,7 @@ Rescue_Camera_lores_size = (Rescue_Camera_size[0] // 4,
                             Rescue_Camera_size[1] // 4)
 Rescue_Camera_Pre_Callback_func = Rescue_Camera_Pre_callback
 
-Linetrace_Camera_PORT = 0
+Linetrace_Camera_PORT = 1
 Linetrace_Camera_Controls = {
     "AfMode": controls.AfModeEnum.Manual,
     "LensPosition": 1.0 / 0.03,
