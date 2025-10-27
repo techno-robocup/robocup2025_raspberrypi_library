e6760aa7d60156038b04a04a1ebad7865036cf35
K10-K10 <keito.tottori@gmail.com>
2025-10-27 18:07:56 +0900

    Debug: YOLO result

--- settings.py
+++ settings.py
@@ -582,7 +582,8 @@ def Rescue_Camera_Pre_callback(request):
 
             yolo_results = MODEL(fixed_image)
             modules.settings.yolo_results = yolo_results
-            cv2.imwrite(f"bin/{str(time.time())}_rescue_result.jpg", yolo_results)
+            result_image = yolo_results[0].plot()
+            cv2.imwrite(f"bin/{time.time():.3f}_rescue_result.jpg", result_image)
             modules.rescue.rescue_loop_func()
 
             last_yolo_time = current_time
